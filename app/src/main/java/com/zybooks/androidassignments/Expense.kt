411e4838fc8900155861303d06172aee67f49808
parekh <w0846147@myscc.ca>
2025-04-04 07:24:49 -0700

    Modified the Expense.kt and added the two new fields

--- app/src/main/java/com/zybooks/androidassignments/Expense.kt
+++ app/src/main/java/com/zybooks/androidassignments/Expense.kt
@@ -2,5 +2,7 @@ package com.zybooks.androidassignments
 
 data class Expense(
     val name: String,
-    val totalAmount: String
+    val totalAmount: String,
+    var currency: String = "CAD",
+    var convertedCost: Double = 0.0
 )
