bd494da5a3e21a8aa611a58a89732cc565bf6b28
parekh <w0846147@myscc.ca>
2025-04-04 08:47:49 -0700

    Created CurrencyService singleton object to handle retrofit setup. Added dependencies for retrofit and Gson. Implemented CurrencyApi interface and CurrencyResponse model.

--- app/src/main/java/com/zybooks/androidassignments/network/CurrencyApi.kt
+++ app/src/main/java/com/zybooks/androidassignments/network/CurrencyApi.kt
@@ -1,9 +1,16 @@
 package com.zybooks.androidassignments.network
 
-import com.zybooks.androidassignments.model.CurrencyResponse
-import retrofit2.http.GET
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
-interface CurrencyApi {
-    @GET("v6/latest/USD")
-    suspend fun getRates(): CurrencyResponse
+object CurrencyApi {
+    private const val BASE_URL = "https://api.exchangerate.host/"
+
+    val api: CurrencyApi by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CurrencyApi::class.java)
+    }
 }
